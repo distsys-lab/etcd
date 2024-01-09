978241e9e47251419b602155b52de66f69f1cb3f
koya <kouyan120706@gmail.com>
2024-01-09 13:41:21 +0900

    Refactor: Cleaned and Organized Disordered Code

--- server/etcdserver/api/rafthttp/transport.go
+++ server/etcdserver/api/rafthttp/transport.go
@@ -339,14 +339,19 @@ func (t *Transport) AddPeer(id types.ID, us []string) {
 	}
 }
 
-func (t *Transport) AddPeer(id types.ID, us []string) {
+func (t *Transport) AddRemote(id types.ID, us []string) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
-
-	if t.peers == nil {
-		panic("transport stopped")
+	if t.remotes == nil {
+		// there's no clean way to shutdown the golang http server
+		// (see: https://github.com/golang/go/issues/4674) before
+		// stopping the transport; ignore any new connections.
+		return
 	}
 	if _, ok := t.peers[id]; ok {
+		return
+	}
+	if _, ok := t.remotes[id]; ok {
 		return
 	}
 	urls, err := types.NewURLs(us)
@@ -355,14 +360,11 @@ func (t *Transport) AddPeer(id types.ID, us []string) {
 			t.Logger.Panic("failed NewURLs", zap.Strings("urls", us), zap.Error(err))
 		}
 	}
-	fs := t.LeaderStats.Follower(id.String())
-	t.peers[id] = startPeer(t, urls, id, fs)
-	addPeerToProber(t.Logger, t.pipelineProber, id.String(), us, RoundTripperNameSnapshot, rttSec)
-	addPeerToProber(t.Logger, t.streamProber, id.String(), us, RoundTripperNameRaftMessage, rttSec)
+	t.remotes[id] = startRemote(t, urls, id)
 
 	if t.Logger != nil {
 		t.Logger.Info(
-			"added remote peer",
+			"added new remote peer",
 			zap.String("local-member-id", t.ID.String()),
 			zap.String("remote-peer-id", id.String()),
 			zap.Strings("remote-peer-urls", us),
